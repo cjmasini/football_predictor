ec1dd3f6207f73e404f3a122faf2e963942ad73a
Altaimorin <31492878+Altaimorin@users.noreply.github.com>
2018-11-30 13:16:48 -0500

    Update readin.py

--- readin.py
+++ readin.py
@@ -6,7 +6,6 @@ def readin():
         line = line.strip().split(",")[1:43]+line.strip().split(",")[-1:-25:-1]
         # for new input: line = line.strip().split(",")[3:]
         line = np.array(list(map(float,line)))    #only take the score part
-        #line = line - np.mean(line)
         stats.append(line)    
 
     result_file = open('2017resultsMatrix.csv','r')
@@ -24,6 +23,12 @@ def readin():
     random.shuffle(whole_data_set)
     stat,result = zip(*whole_data_set)
     stat,result = np.array(stat), np.array(result)
+    
+    #normalization
+    stat = (stat-stat.min(axis=0))/(stat.max(axis=0)-stat.min(axis=0))-0.5
+    result = (result-result.min(axis=0))/(result.max(axis=0)-result.min(axis=0))-0.5
+ 
+    
     X_train, X_test, Y_train, Y_test =         stat[:int(0.75*counts)],stat[int(0.75*counts):],result[:int(0.75*counts)],result[int(0.75*counts):]
     return  X_train, X_test, Y_train, Y_test
 
